7a7890826039556d1630ace0b7d695e3f9ca157e
CowBreado <Joselorenzoveracruz@gmail.com>
2025-02-07 21:33:29 +0800

    ITECC06 First Activity

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-export default function App() {
+
+
+
+const Ligma = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style =  {mystyles.container}>
+      <View style = {mystyles.box1}>
+      </View>
+      
+      <View style = {mystyles.box2}>
+      </View>
+
+      <View style = {mystyles.box3}>
+      </View>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+
+
+const mystyles = StyleSheet.create({
+  container:{
+    flex:1,
     justifyContent: 'center',
+    alignItems:'center'
   },
+  
+  box1:{
+    backgroundColor:'blue',
+    width: 50,
+    height: 50
+  },
+  box2:{
+    backgroundColor:'red',
+    width:80,
+    height:80
+  },
+
+
+  })
+
+
+
+
+const JohnXina = () => {
+  return (
+    <View style={textStyle.container}>
+      <Text>Jai Jinendra</Text>
+    </View>
+
+  )
+}
+const textStyle = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
+
+
+
+ 
+
+
+
+  
+
+
+
+
+
+export default Ligma
+
+const styles = StyleSheet.create({})
